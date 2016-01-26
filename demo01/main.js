447c030cb30f5ae3dd12c6b19a9c406eb225dab5
peter-lenovol <chunhuipeter@163.com>
2016-01-26 21:53:34 +0800

    remove password

--- demo01/main.js
+++ demo01/main.js
@@ -7,7 +7,7 @@ var Component=require('./com.react');
 //测试props功能
 describe("test helloworld props",function(){
 	it("check props",function(){
-		var helloworld=TestUtils.renderIntoDocument(<Component text='你爱吃橘子'></Component>)
+		var helloworld=TestUtils.renderIntoDocument(<Component text='1你爱吃橘子'></Component>)
 		var text=ReactDOM.findDOMNode(helloworld).textContent;
 		expect(text).toContain('爱吃橘子');
 	})
